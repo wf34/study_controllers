9258e0989efc13d632a5eff043dd4070afa0e0a0
wf34 <wf34@users.noreply.github.com>
2024-12-22 14:51:44 +0200

    better state monitor

--- controllers/state_monitor.py
+++ controllers/state_monitor.py
@@ -1,5 +1,5 @@
 import os
-from typing import Union, List
+from typing import Union, List, Optional
 
 import numpy as np
 from pydantic import BaseModel
@@ -8,40 +8,49 @@ from pydrake.systems.framework import EventStatus
 from pydrake.multibody.tree import MultibodyForces
 from pydrake.multibody.math import SpatialForce
 
+from pydrake.all import (
+    RigidTransform,
+)
+
 import numpy as np
 
-class Vector3(BaseModel):
+from catalog import get_transl2d_from_transform, get_rot2d_from_transform
+
+class Vector2(BaseModel):
     x: float
-    y: float
     z: float
 
     def instantiate_from_arr(arr: Union[np.array, List]):
         if isinstance(arr, np.ndarray):
             arr = arr.tolist()
-        if len(arr) < 2 or len(arr) > 3:
-            raise Exception('size err')
-        if len(arr) == 2:
-            arr = [arr[0]] + [0.] + [arr[1]]
-
         dct = {}
-        for i, n in zip(range(3), ['x', 'y', 'z']):
+        for i, n in zip(range(2), ['x', 'z']):
             dct[n] = arr[i]
 
-        # because in jac in was ry, tx, tz, but for vis. purposes we prefer normal order: x,y,z
-        dct['y'], dct['x'] = dct['x'], dct['y']
-
-        return Vector3(**dct)
+        return Vector2(**dct)
 
 
 class Datum(BaseModel):
     time: float
-    reaction_forces: Vector3
+    pe_s: Vector2
+    f_s: Vector2
 
 
 class StateMonitor:
-    def __init__(self, path, plant, diagram):
+    def __init__(self, path, plant, diagram, ttrajectory, ctrajectory, meshcat):
         self._plant = plant
+        self._iiwa = plant.GetModelInstanceByName("iiwa")
+        gripper = plant.GetBodyByName("body")
+        self._gripper_body_instance = gripper.index()
+        self._shelf_body_instance = plant.GetBodyByName("shelf_body").index()
         self._diagram = diagram
+
+        self._station = diagram.GetSubsystemByName("station")
+
+        self.torque_trajectory = ttrajectory
+        self.cart_trajectory = ctrajectory
+        self._meshcat = meshcat
+        self.X_Wshelf = None
         
         if os.path.exists(path):
             assert not os.path.isdir(path)
@@ -50,37 +59,65 @@ class StateMonitor:
 
         self._file = open(path, 'a')
         self._file.write('[')
-        # self._counter = 0
+
+    def get_mode(self) -> str:
+        if self.torque_trajectory is not None and self.cart_trajectory is None:
+            return 'stiffness'
+        elif self.torque_trajectory is None and self.cart_trajectory is not None:
+            return 'hybrid'
+        else:
+            raise Exception('unreachable')
+
+    def get_goal_pose(self, root_context) -> RigidTransform:
+        time = root_context.get_time()
+        if 'stiffness' == self.get_mode():
+            q_desired = self.torque_trajectory.value(time).T.ravel()
+
+            plant_context = self._plant.GetMyContextFromRoot(root_context)
+            saved_internal_coords = self._plant.GetPositions(plant_context, self._iiwa)
+            self._plant.SetPositions(plant_context, self._iiwa, q_desired)
+            X_WG = self._plant.EvalBodyPoseInWorld(plant_context, self._plant.GetBodyByName("body"))
+            self._plant.SetPositions(plant_context, self._iiwa, saved_internal_coords)
+            return X_WG
+
+        elif 'hybrid' == self.get_mode():
+            return self.cart_trajectory.GetPose(time)
+
 
     def callback(self, root_context):
-        force_sensor_system = self._diagram.GetSubsystemByName('ForceSensor')
-        #self._diagram.GetPort('trajectory')
-        #self._counter += 1
-        #if 0 != self._counter % 100:
-        #    return
+        current_time = root_context.get_time()
+        if 2. <= current_time and current_time < 12.:
+            station_context = self._station.GetMyContextFromRoot(root_context)
+            poses = self._station.GetOutputPort('body_poses').Eval(station_context)
+            if self.X_Wshelf is None:
+                self.X_Wshelf = poses[self._shelf_body_instance]
 
-        #end_effector = self._plant.GetBodyByName("body")
-        #plant_context = self._plant.GetMyContextFromRoot(root_context)
-        sensor_context = force_sensor_system.GetMyContextFromRoot(root_context)
-        #X_WB = nut.EvalPoseInWorld(nut_context)
-        #contact_results = self._plant.get_contact_results_output_port().Eval(nut_context)
-        #multibody_forces = MultibodyForces(plant=self._plant)
-        #forces = MultibodyForces(plant=self._plant)
-        #self._plant.CalcForceElementsContribution(context=nut_context, forces=forces)
-        #tau = self._plant.CalcGeneralizedForces(context=nut_context, forces=forces)
-        #empty = np.array([0.]*6)
-        #empty[-1] = np.max(tau) * 1.e+3
-        sensed_reaction_force = force_sensor_system.GetOutputPort('sensed_force_out').Eval(sensor_context)
+            R_shelfW = get_rot2d_from_transform( self.X_Wshelf )  # because 3D->2D, XZ->XY
 
-        datum = Datum(time=root_context.get_time(),
-                      reaction_forces=Vector3.instantiate_from_arr(sensed_reaction_force))
+            pose_desired = self.get_goal_pose(root_context)
+            plant_context = self._plant.GetMyContextFromRoot(root_context)
 
-        if self._file:
-            self._file.write(datum.model_dump_json(exclude_none=True))
-            self._file.write(',')
-            self._file.flush()
+            pose_measured = poses[self._gripper_body_instance]
 
-        #print(self._plant.GetPositions(plant_context))
-        #print(self._plant.GetPositions(plant_context, self._plant.GetModelInstanceByName("iiwa")))
+            pd_WG = get_transl2d_from_transform(pose_desired)
+            pm_WG = get_transl2d_from_transform(pose_measured)
+
+            pd_WS = R_shelfW @ pd_WG
+            pm_WS = R_shelfW @ pm_WG
+
+            pe_S = pd_WS - pm_WS
+
+            force_sensor_system = self._diagram.GetSubsystemByName('ForceSensor')
+            sensor_context = force_sensor_system.GetMyContextFromRoot(root_context)
+            sensed_reaction_force = force_sensor_system.GetOutputPort('sensed_force_out').Eval(sensor_context)[1:]
+
+            datum = Datum(time=root_context.get_time(),
+                          pe_s=Vector2.instantiate_from_arr(pe_S),
+                          f_s=Vector2.instantiate_from_arr(sensed_reaction_force))
+
+            if self._file:
+                self._file.write(datum.model_dump_json(exclude_none=True))
+                self._file.write(',')
+                self._file.flush()
 
         return EventStatus.DidNothing()
