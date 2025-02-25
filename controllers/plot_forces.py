7d2b86f584f4600cd0ef225a648d14da8fdf6426
wf34 <wf34@users.noreply.github.com>
2025-02-26 01:30:06 +0200

    plot_forces: forces graph

--- controllers/plot_forces.py
+++ controllers/plot_forces.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 import os
+import operator
 import json
 import typing
+import math
 
 import numpy as np
 from tap import Tap
+#import matplotlib
+#matplotlib.use('GTK3Agg')
 from matplotlib import pyplot as plt
 
 from state_monitor import Datum
+from planning import TurnStage
 
 from pydrake.all import (
     RigidTransform,
@@ -20,6 +25,7 @@ class PlotterArgs(Tap):
     labels: typing.List[str] = []
     mode: typing.Literal['pe', 'f', 'p']
     sparse_load: bool = False
+    destination: typing.Optional[str] = None
 
     def process_args(self):
         if len(list(map(os.path.isfile, self.input_files))) != len(self.input_files):
@@ -37,7 +43,7 @@ def load_datums(input_file: str, do_sparse_load: bool = False):
         json_struct = json.loads(file_contents)
         datums_cnt = 0
         for datum_raw in json_struct:
-            if not do_sparse_load or 0 == datums_cnt % 10:
+            if not do_sparse_load or 0 == datums_cnt % 25:
                 datums.append(Datum(**datum_raw))
             datums_cnt += 1
     return datums
@@ -47,50 +53,120 @@ def plot(args: PlotterArgs):
     per_file_datums = list(map(lambda x: load_datums(x, args.sparse_load), args.input_files))
     
     trajectories_amount = len(per_file_datums)
-    print(trajectories_amount, len(args.labels), '/////')
 
     fig = plt.figure()
     ax = fig.subplots(nrows=1, ncols=1)
     ax_secondary = ax.twinx()
     ax_secondary.yaxis.set_ticks(np.arange(0, 4))
 
-    NUM_COLORS = trajectories_amount
-    cm = plt.get_cmap('flag')
-    ax.set_prop_cycle(color=[cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
-
-    is_first = True
+    #NUM_COLORS = trajectories_amount
+    #cm = plt.get_cmap('flag')
+    #ax.set_prop_cycle(color=[cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)])
+    main_colors = ['r', 'k', 'green']
+    assert trajectories_amount <= len(main_colors)
     lines = []
-    for datums, label in zip(per_file_datums, args.labels):
+    labels = []
+    starting_pitch = None
+    pitch_for_vis = lambda x: np.degrees(x - starting_pitch)
+    first_trajectory = True
+
+    for datums, label, mc in zip(per_file_datums, args.labels, main_colors):
         t = []
-        tn = []
+        t_by_stage = {}
+        tn_by_stage = {}
         pitches = []
+        Ms = []
         for d in datums:
             t.append(d.time)
-            tn.append(d.turn)
-            pitches.append(d.valve_pitch_angle)
+            if d.force_sensed is not None:
+                Ms.append(d.force_sensed.x)
+            else:
+                Ms.append(None)
 
-        if is_first:
-            l1 = ax_secondary.plot(t, tn, label='turn count', color='k')
-            is_first = False
-            lines.extend(l1)
+            t_by_stage.setdefault(d.stage, []).append(d.time)
+            tn_by_stage.setdefault(d.stage, []).append(d.turn + 1 if d.stage != TurnStage.RETRACT else d.turn)
+            if starting_pitch is None:
+                pitches.append(0.)
+                starting_pitch = d.valve_pitch_angle
+            else:
+                pitches.append(pitch_for_vis(d.valve_pitch_angle))
 
         if 'pe' == args.mode:
             pass
         elif 'f' == args.mode:
-            pass
+            if True:
+                inds = []
+                ind_ends = []
+                turn_id_to_screw_start_time = lambda i:  22. * i + 6.
+                turn_id_to_screw_end_time = lambda i: turn_id_to_screw_start_time(i) + 10.
+                for x in range(3):
+                    cut_val = turn_id_to_screw_start_time(x)
+                    cut_val_end = turn_id_to_screw_end_time(x)
+                    find_index = lambda cut_value : next(map(operator.itemgetter(1), filter(lambda x: math.fabs(x[0] - cut_value) < 1.e-3, zip(t, range(len(t))))))
+                    inds.append(find_index(cut_val))
+                    ind_ends.append(find_index(cut_val_end))
+
+                times_ = []
+                moments = []
+                min_len = None
+                for i in range(3):
+                    times_.append(t[inds[i]: ind_ends[i]])
+                    moments.append(Ms[inds[i]: ind_ends[i]])
+                    cur_len = len(moments[-1])
+                    if min_len is None or cur_len < min_len:
+                        min_len = cur_len
+                for i in range(3):
+                    times_[i] = times_[i][1:min_len]
+                    moments[i] = moments[i][1:min_len]
+
+                moments = np.mean(moments, axis=0)
+                print(moments.shape)
+                lf = ax.plot(times_[0], moments, label=label, color=mc)
+                lines.extend(lf)
+                labels.append(lf[0].get_label())
+                if first_trajectory:
+                    first_trajectory = False
+                    lh = ax.axhline(y=-.2, color=main_colors[-1], linestyle='-.', label='target_moment')
+                    lines.append(lh)
+                    labels.append(lh.get_label())
+            else:
+                lf = ax.plot(t, Ms, label=label, color=mc)
+                lines.extend(lf)
+                labels.append(lf[0].get_label())
+
         elif 'p' == args.mode:
-            l2 = ax.plot(t, pitches, label=f'{label}/pitch')
+            if first_trajectory:
+                for stage, c in zip([TurnStage.APPROACH, TurnStage.SCREW, TurnStage.RETRACT],
+                                    ['orange', 'yellow', 'cyan']):
+                                    #[(1, .49, 0.), (1, 1, 0), (0., .54, .54)]):
+                    l_sec = ax_secondary.scatter(t_by_stage[stage], tn_by_stage[stage], color=c, s=2**8, alpha=0.33, edgecolors='none')
+                    lines.append(l_sec)
+                    labels.append(f'{stage.value} at turn #')
+
+                first_trajectory = False
+            l2 = ax.plot(t, pitches, label=f'{label}', color=mc)
             lines.extend(l2)
+            labels.append(lines[-1].get_label())
         else:
             raise Exception('unreachable')
 
-    for offset in range(3):
-        real_offset = offset * 22
-        ax.axvline(real_offset + 6., color='red', linestyle='-.', alpha=1)
-        ax.axvline(real_offset + 16., color='green', linestyle='-.', alpha=1)
+    if args.mode == 'p':
+        ax.set_zorder(ax_secondary.get_zorder() + 1)
+        ax.patch.set_visible(False)
+        ax_secondary.set_ylabel('Turn #')
+        ax.set_ylabel('Nut Turning Progress, degrees')
+    elif args.mode == 'f':
+        ax.set_ylabel('Nut-Gripper Average Contact Moment, N•m')
+    ax.legend(lines, labels)
+    ax.set_xlabel('Simulation Time, seconds')
+    plt.tight_layout()
 
-    ax.legend(lines, [l.get_label() for l in lines])
-    plt.show()
+    if args.destination is not None:
+        assert args.destination.endswith('.eps')
+        fig.savefig(args.destination, format='eps')
+    else:
+        plt.show()
+
 
 
 if __name__ == '__main__':
